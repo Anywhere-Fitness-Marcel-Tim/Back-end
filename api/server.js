4b0a77c0afc8c7cfd7f2c19d33b2cb622c3d6e1f
marcel <marcel_rodrgez@yahoo.com>
2022-02-28 09:45:06 -0500

    added eslint, created server files, scaffolded codebase

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router')
+const classRouter = require('./classes/classes-router')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+server.use(morgan('dev'))
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', usersRouter)
+server.use('/api/classes', classRouter)
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        custom: 'The coding gods are mad',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = server;
