7a48c4294f1810532771a30f5ae1d3e2c2147b89
marcel <marcel_rodrgez@yahoo.com>
2022-03-02 07:58:28 -0500

    Deploy commit 3

--- knexfile.js
+++ knexfile.js
@@ -25,13 +25,7 @@ module.exports = {
         migrations: {
             directory: './data/migrations',
         },
-    },
-    seeds:{
-        directory: './data/seeds',
-    },
-    pool: {
-        afterCreate: (conn, done) => {
-            conn.run('PRAGMA foreign_keys = ON', done)
-        },
+        seeds:{ directory: './data/seeds' },
+        pool: { min: 2, max: 10},
     },
 }
